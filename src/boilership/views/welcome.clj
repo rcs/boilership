b716eea2ee7af0ce3853540a414ad8a362f5a037
Ryan Sorensen <rcsorensen@gmail.com>
2012-04-29 18:02:48 -0700

    Working with passing url param to boil

--- src/boilership/views/welcome.clj
+++ src/boilership/views/welcome.clj
@@ -1,18 +1,16 @@
 (ns boilership.views.welcome
-  (:require [boilership.views.common :as common]
-            [noir.content.getting-started])
+  (:require [boilership.views.common :as common])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to boilership"]))
-(defpage "/my-page" []
-         (html
-           (.process
-             (de.l3s.boilerpipe.sax.HTMLHighlighter/newExtractingInstance)
-             (new java.net.URL "http://www.cnn.com/2012/04/28/world/bin-laden-death-anniversary/index.html?hpt=hp_c1")
-             de.l3s.boilerpipe.extractors.CommonExtractors/ARTICLE_EXTRACTOR
-             )
-           ))
+(defn extract-article
+  ([url]
+   (.process
+     (de.l3s.boilerpipe.sax.HTMLHighlighter/newExtractingInstance)
+     (new java.net.URL url)
+     de.l3s.boilerpipe.extractors.CommonExtractors/ARTICLE_EXTRACTOR)))
 
+
+(defpage "/boil/*" {url :*}
+         (html
+           (extract-article url)))
